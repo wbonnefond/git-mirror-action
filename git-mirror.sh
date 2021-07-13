d61316660a3ee7bb713f3e3ed71ad241abb85604
Ward Bonnefond <wbonnefond@gmail.com>
2021-07-13 01:28:16 -0400

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -11,8 +11,12 @@ echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
 git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
-git remote set-url --push origin "$DESTINATION_REPO"
+#git remote set-url --push origin "$DESTINATION_REPO"
+git remote add mirror "$DESTINATION_REPO"
+git config --unset-all remote.origin.fetch
+git config --add remote.origin.fetch "+refs/heads/*:refs/heads/*"
+git config --add remote.origin.fetch "+refs/tags/*:refs/tags/*"
 git fetch -p origin
 # Exclude refs created by GitHub for pull request.
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-git push --mirror
+git push mirror
